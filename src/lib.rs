f33402a1c0b5d6b13d6b5958df788ab3a4bac03e
Richard Palethorpe <richiejp@f-m.fm>
2017-09-17 17:56:41 +0200

    Move builtins and add cons, car, cdr
    Or atleast add functions resembling cons, car and cdr. Seeing as we aren't
    actually using linked lists.

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,6 @@
 extern crate fnv;
 
 use std::slice::Iter;
-use std::collections::HashMap;
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::fmt;
@@ -25,6 +24,9 @@ use fnv::FnvHashMap;
 
 mod tokenizer;
 use tokenizer::*;
+
+mod builtins;
+use builtins::*;
 
 #[derive(Debug, Clone)]
 pub enum Inner {
@@ -124,8 +126,29 @@ impl Sexp {
         }
     }
 
+    fn from(lst: &[Inner]) -> Sexp {
+        Sexp {
+            delim: '(',
+            lst: Vec::from(lst),
+        }
+    }
+
     fn push(&mut self, child: Inner) {
         self.lst.push(child);
+    }
+
+    fn car(&self) -> Inner {
+        match self.lst.first() {
+            Some(car) => car.clone(),
+            None => Inner::nil(),
+        }
+    }
+
+    fn cdr(&self) -> Inner {
+        match self.lst.split_first() {
+            Some((_, rest)) => Inner::Sxp(Sexp::from(rest)),
+            _ => Inner::nil(),
+        }
     }
 
     fn new_inner_sxp(&mut self, delim: char) -> &mut Sexp {
@@ -245,173 +268,6 @@ impl fmt::Display for UserFunc {
     }
 }
 
-macro_rules! def_builtin {
-    ($name:expr, $rname:ident, $evaled:ident, $lsp:ident, $args:ident; $fn_body:block ) => (
-        #[derive(Clone)]
-        struct $rname { }
-        impl Func for $rname {
-            fn eval_args(&self) -> EvalOption {
-                EvalOption::$evaled
-            }
-
-            fn name(&self) -> &'static str {
-                $name
-            }
-
-            fn call(&self, $lsp: &mut Lsp, $args: &mut Iter<Inner>) -> Result<Inner, String> {
-                $fn_body
-            }
-        }
-    )
-}
-
-def_builtin! { "-", MinusBuiltin, Evaluated, _lsp, args; {
-    let mut res = 0;
-
-    if let Some(arg) = args.next() {
-        match arg {
-            &Inner::Int(i) => res = i,
-            _ => return Err(format!("Expected int, but found {:?}", arg)),
-        }
-    }
-
-    if let Some(arg) = args.next() {
-        match arg {
-            &Inner::Int(i) => res -= i,
-            _ => return Err(format!("Expected int, but found {:?}", arg)),
-        }
-    } else {
-        res = -res;
-    }
-
-    while let Some(arg) = args.next() {
-        match arg {
-            &Inner::Int(i) => res -= i,
-            _ => return Err(format!("Expected int, but found {:?}", arg)),
-        };
-    }
-
-    Ok(Inner::Int(res))
-}}
-
-def_builtin! { "+", PlusBuiltin, Evaluated, _lsp, args; {
-    let mut res = 0;
-
-    while let Some(arg) = args.next() {
-        match arg {
-            &Inner::Int(i) => res += i,
-            _ => return Err(format!("Expected int, but found {:?}", arg)),
-        };
-    }
-
-    Ok(Inner::Int(res))
-}}
-
-def_builtin! { "quote", QuoteBuiltin, Unevaluated, _lsp, args; {
-    let argt = (args.next(), args.next());
-    match argt {
-        (Some(arg), None) => Ok(arg.clone()),
-        _ => Err(format!("Wrong number of arguments; quote only accepts one")),
-    }
-}}
-
-def_builtin! { "lambda", LambdaBuiltin, Unevaluated, _lsp, args; {
-    match (args.next(), args.next()) {
-        (Some(&Inner::Sxp(ref args_sxp)), Some(body)) => {
-            let largs: Result<Vec<ArgSpec>, String> = args_sxp.lst.iter().map(
-                |arg| -> Result<ArgSpec, String> {
-                    match arg {
-                        &Inner::Sym(ref name) => Ok(ArgSpec::new(name.to_owned())),
-                        _ => Err(format!("Lambda arguments must be symbols")),
-                    }
-                }
-            ).collect();
-
-            Ok(Inner::Lambda(
-                UserFunc::new("".to_owned(), largs?, match body {
-                    &Inner::Ref(ref iref) => iref.clone(),
-                    _ => body.clone().into_ref(),
-                })
-            ))
-        },
-        _ => Err(format!("(lambda ([args]) [body])")),
-    }
-}}
-
-def_builtin! { "defalias", DefaliasBuiltin, Evaluated, lsp, args; {
-    let name = match args.next() {
-        Some(&Inner::Sym(ref name)) => name,
-        _ => return Err(format!("defalias expected symbol")),
-    };
-
-    let mut fun = match args.next() {
-        Some(&Inner::Lambda(ref lmbda)) => lmbda.clone(),
-        _ => return Err(format!("defalias expected lambda")),
-    };
-
-    fun.name = name.to_owned();
-    lsp.globals.reg_fn(fun);
-    Ok(Inner::Sym(name.to_owned()))
-}}
-
-def_builtin! { "print", PrintBuiltin, Evaluated, _lsp, args; {
-    let mut s = String::new();
-    let _res = fmt_iter('(', args, &mut s);
-    println!("{}", &s);
-    Ok(Inner::Str(s))
-}}
-
-def_builtin! { "exit", ExitBuiltin, Unevaluated, _lsp, _args; {
-    Ok(Inner::Sym("exit".to_owned()))
-}}
-
-def_builtin! { "progn", PrognBuiltin, Evaluated, _lsp, args; {
-    if let Some(val) = args.last() {
-        Ok(val.clone())
-    } else {
-        Err(format!("progn requires one or more arguments"))
-    }
-}}
-
-def_builtin! { "if", IfBuiltin, Unevaluated, lsp, args; {
-    if let Some(cond) = args.next() {
-        let then = match args.next() {
-            Some(then) => then,
-            None => return Err(format!("if requires a THEN argument")),
-        };
-
-        if !lsp.eval_inner(cond)?.is_nil() {
-                lsp.eval_inner(then)
-        } else {
-            let mut ret = if let Some(lelse) = args.next() {
-                lsp.eval_inner(lelse)?
-            } else {
-                Inner::nil()
-            };
-
-            while let Some(lelse) = args.next() {
-                ret = lsp.eval_inner(lelse)?;
-            }
-
-            Ok(ret)
-        }
-    } else {
-        Err(format!("if requires a COND argument"))
-    }
-}}
-
-def_builtin! { "eq", EqBuiltin, Evaluated, _lsp, args; {
-    if let (Some(left), Some(right)) = (args.next(), args.next()) {
-        if left == right {
-            Ok(Inner::t())
-        } else {
-            Ok(Inner::nil())
-        }
-    } else {
-        Err(format!("eq requires two arguments"))
-    }
-}}
-
 struct Namespace {
     funcs: FnvHashMap<String, Rc<Func>>,
     vars: FnvHashMap<String, Inner>,
@@ -458,6 +314,9 @@ impl Lsp {
         g.reg_fn(PrognBuiltin { });
         g.reg_fn(IfBuiltin { });
         g.reg_fn(EqBuiltin { });
+        g.reg_fn(ConsBuiltin { });
+        g.reg_fn(CarBuiltin { });
+        g.reg_fn(CdrBuiltin { });
 
         g.reg_var("t".to_owned(), &Inner::t());
         g.reg_var("nil".to_owned(), &Inner::nil());
