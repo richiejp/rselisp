5390b5df8400d84f8a372a61c5bfeb604446d499
Richard Palethorpe <richiejp@f-m.fm>
2017-09-17 19:04:14 +0200

    Dodgy perf test for lists

--- tests/perf.rs
+++ tests/perf.rs
@@ -23,3 +23,27 @@ fn fib(b: &mut Bencher) {
 
     b.iter(|| assert_eq!(Ok(Inner::Int(10946)), lsp.eval(&ast)));
 }
+
+#[bench]
+fn cons(b: &mut Bencher) {
+    let mut lsp = Lsp::new();
+    let src = r#"
+(defalias 'repeat
+  (lambda (a c)
+    (if (eq c 0)
+	a
+      (cons a (repeat a (- c 1))))))
+
+(defalias 'prn-lst
+  (lambda (l)
+    (if (listp l) (progn
+		    (print (car l))
+		    (prn-lst (cdr l)))
+      (print l))))
+
+(prn-lst (repeat 'a 100))
+"#.to_owned();
+    let ast = lsp.read(&src).unwrap();
+
+    b.iter(|| lsp.eval(&ast));    
+}
