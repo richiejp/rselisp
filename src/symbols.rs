7b5e05a6cd2de6b59c06243db0d1e853fe01aeea
Richard Palethorpe <richiejp@f-m.fm>
2017-11-04 11:25:14 +0100

    Revert to plain integer as Atom

--- src/symbols.rs
+++ src/symbols.rs
@@ -6,69 +6,37 @@ use std::usize;
 
 use super::*;
 
-#[derive(Clone)]
+pub static NIL: Atom = Atom { indx: 0 };
+pub static LAMBDA: Atom = Atom { indx: 1 };
+pub static MACRO: Atom = Atom { indx: 2 };
+
+
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub struct Atom {
     indx: usize,
-    reg: Weak<RefCell<AtomRegistry>>,
 }
 
 impl Atom {
-    fn default() -> Atom {
-        Atom { indx: usize::MAX, reg: Weak::new() }
-    }
-}
-
-impl PartialEq for Atom {
-    fn eq(&self, other: &Atom) -> bool {
-        self.indx == other.indx
-    }
-}
-
-impl Eq for Atom {
-}
-
-impl fmt::Display for Atom {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let reg = self.reg.upgrade().unwrap();
-        let reg = reg.borrow();
-        write!(f, "{}", reg.stringify(self))
-    }
-}
-
-impl fmt::Debug for Atom {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Atom({}, {})", self.indx, self)
+    fn new(indx: usize) -> Atom {
+        Atom { indx: indx }
     }
 }
 
 pub struct AtomRegistry {
     table: Vec<String>,
     rev_table: FnvHashMap<String, Atom>,
-    self_ref: Weak<RefCell<AtomRegistry>>,
-
-    pub NIL: Atom,
-    pub LAMBDA: Atom,
-    pub MACRO: Atom,
 }
 
 impl AtomRegistry {
-    fn with_capacity(capacity: usize) -> Rc<RefCell<AtomRegistry>> {
+    fn with_capacity(capacity: usize) -> AtomRegistry {
         let cap = capacity + 3;
-        let me = Rc::new(RefCell::new(AtomRegistry {
+        let mut me = AtomRegistry {
             table: Vec::with_capacity(cap),
             rev_table: FnvHashMap::with_capacity_and_hasher(cap, Default::default()),
-            self_ref: Weak::new(),
-            NIL: Atom::default(),
-            LAMBDA: Atom::default(),
-            MACRO: Atom::default(),
-        }));
-        {
-            let mut m = me.borrow_mut();
-            m.self_ref = Rc::downgrade(&me);
-            m.NIL = m.atomize("nil");
-            m.LAMBDA = m.atomize("lambda");
-            m.MACRO = m.atomize("macro");
-        }
+        };
+        me.atomize("nil");
+        me.atomize("lambda");
+        me.atomize("macro");
         me
     }
 
@@ -79,14 +47,13 @@ impl AtomRegistry {
             self.table.push(name.into());
             let atom = Atom {
                 indx: self.table.len() - 1,
-                reg: self.self_ref.clone(),
             };
             self.rev_table.insert(name.into(), atom.clone());
             atom
         }
     }
 
-    pub fn stringify(&self, atom: &Atom) -> &str {
+    pub fn stringify(&self, atom: Atom) -> &str {
         &self.table[atom.indx]
     }
 }
@@ -104,43 +71,40 @@ mod tests {
 
     #[test]
     fn builtins() {
-        let reg = AtomRegistry::with_capacity(1);
-        let mut reg = reg.borrow_mut();
+        let mut reg = AtomRegistry::with_capacity(1);
 
         let atm = reg.atomize("nil");
-        assert_eq!(reg.NIL, atm);
+        assert!(NIL == atm);
         let _cdr = reg.atomize("cdr");
         let atm = reg.atomize("lambda");
-        assert_eq!(reg.LAMBDA, atm);
+        assert!(LAMBDA == atm);
         let atm = reg.atomize("macro");
-        assert_eq!(reg.MACRO, atm);
+        assert!(MACRO == atm);
     }
 
     #[test]
     fn same_atom() {
         let s = "magit-imenu--repolist-extract-index-name-function";
-        let reg = AtomRegistry::with_capacity(1);
-        let mut reg = reg.borrow_mut();
+        let mut reg = AtomRegistry::with_capacity(1);
 
         let atm = reg.atomize(s);
-        assert_eq!(reg.atomize(s), atm);
-        assert_eq!(reg.stringify(&atm), s);
+        assert!(reg.atomize(s) == atm);
+        assert!(reg.stringify(atm) == s);
     }
 
     #[test]
     fn different_atoms() {
         let s = "car";
         let t = "let";
-        let reg = AtomRegistry::with_capacity(2);
-        let mut reg = reg.borrow_mut();
+        let mut reg = AtomRegistry::with_capacity(2);
 
         let u = reg.atomize(s);
         let v = reg.atomize(t);
-        assert_eq!(reg.stringify(&v), t);
-        assert_eq!(reg.stringify(&u), s);
-        assert_eq!(reg.atomize(t), v);
-        assert_eq!(reg.atomize(s), u);
-        assert_ne!(u, v);
-        assert_ne!(reg.stringify(&u), reg.stringify(&v));
+        assert!(reg.stringify(v) == t);
+        assert!(reg.stringify(u) == s);
+        assert!(reg.atomize(t) == v);
+        assert!(reg.atomize(s) == u);
+        assert!(u != v);
+        assert!(reg.stringify(u) != reg.stringify(v));
     }
 }
